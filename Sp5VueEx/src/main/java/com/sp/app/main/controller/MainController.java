90553d4130456728464251bc5d0b2f8fad94e1f0
dgkim <dgkim@dgkim>
2020-05-20 09:44:55 +0900

    init

---
+++ Sp5VueEx/src/main/java/com/sp/app/main/controller/MainController.java
@@ -0,0 +1,37 @@
+package com.sp.app.main.controller;
+
+import java.util.Locale;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * @author 김동건 (dgkim@bsgglobal.com)
+ */
+@Controller
+public class MainController {
+
+	private static final Logger logger = LogManager.getLogger(MainController.class);
+	
+	@Autowired
+	Environment env;
+	
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String main(Locale locale, Model model) {
+		logger.info("홈 Welcome Main! The client locale is {}.", locale);
+		logger.info("현재환경 {}.", env.getProperty("spring.profiles.active"));
+		try {
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return "main";
+	}
+	
+}
