1901e09149f90181ded7a9313b1b919c8772952d
dgkim <dgkim@dgkim>
2020-05-20 09:46:09 +0900

    init

---
+++ vuejsex/WebContent/src/components/TodoList.vue
@@ -0,0 +1,68 @@
+<template>
+  <section>
+    <transition-group name="list" tag="ul">
+      <li v-for="(item, index) in propsdata" v-bind:key="item" class="shadow">
+        <i class="checkBtn fas fa-check" aria-hidden="true"></i>
+        {{item}}
+        <span class="removeBtn" type="button" v-on:click="removeTodo(item, index)">
+          <i class="far fa-trash-alt" aria-hidden="true"></i>
+        </span>
+      </li>
+    </transition-group>
+
+  </section>
+</template>
+
+<script>
+export default {
+  props: ['propsdata'],
+  data(){
+    return{
+      todoItems : []
+    }
+  },
+  methods: {
+    removeTodo(todoItem, index) {
+      this.$emit('removeTodo', todoItem, index);
+    }
+  }
+}
+</script>
+
+<style scoped>
+  ul {
+    list-style-type: none;
+    padding-left: 0px;
+    margin-top: 0;
+    text-align: left;
+  }
+  li {
+    display: flex;
+    min-height: 50px;
+    height: 50px;
+    line-height: 50px;
+    margin: 0.5rem 0;
+    padding: 0 0.9rem;
+    background: white;
+    border-radius: 5px;
+  }
+  .checkBtn {
+    line-height: 45px;
+    color: #62acde;
+    margin-right: 5px;
+  }
+  .removeBtn {
+    margin-left: auto;
+    color: #de4343;
+  }
+  .list-move {
+    transition: 1s;
+  }
+  .list-enter-active, .list-leave-active {
+    transition: all 1s;
+  }
+  .list-enter, .list-leave-to {
+    opacity: 0;
+    transform: translateY(30px);
+  }
+</style>
