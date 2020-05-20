31c26415935eb7a02610f6526fb9b184bec4abc4
dgkim <dgkim@dgkim>
2020-05-20 10:40:23 +0900

    11

--- Sp5VueEx/src/main/webapp/resources/src/components/Left.vue
+++ Sp5VueEx/src/main/webapp/resources/src/components/Left.vue
@@ -4,19 +4,19 @@
       <li class="active"><a href="#">Overview <span class="sr-only">(current)</span></a></li>
       <li><a href="#">Reports</a></li>
       <li><a href="#">Analytics</a></li>
-      <li><a href="#">Export</a></li>
+      <li><a href="#">nav1-4</a></li>
     </ul>
     <ul class="nav nav-sidebar">
-      <li><a href="">Nav item</a></li>
-      <li><a href="">Nav item again</a></li>
-      <li><a href="">One more nav</a></li>
-      <li><a href="">Another nav item</a></li>
-      <li><a href="">More navigation</a></li>
+      <li><a href="">nav2-1</a></li>
+      <li><a href="">nav2-2</a></li>
+      <li><a href="">nav2-3</a></li>
+      <li><a href="">nav2-4</a></li>
+      <li><a href="">nav2-5</a></li>
     </ul>
     <ul class="nav nav-sidebar">
-      <li><a href="">Nav item again</a></li>
-      <li><a href="">One more nav</a></li>
-      <li><a href="">Another nav item</a></li>
+      <li><a href="">nav3-1</a></li>
+      <li><a href="">nav3-2</a></li>
+      <li><a href="">nav3-3</a></li>
     </ul>
   </div>
 </template>
